1366eb0603f3091999ec4d4123528709a95bfa16
Dhruv <dhruv.g123@gmail.com>
2024-07-23 21:10:42 -0700

    added car image

--- car.js
+++ car.js
@@ -21,6 +21,9 @@ class Car{
             );
         }
         this.controls=new Controls(controlType);
+
+        this.img=new Image();
+        this.img.src="car.png";
     }
 
     update(roadBorders,traffic){
@@ -123,19 +126,18 @@ class Car{
     }
 
     draw(ctx,color){
-        if(this.damaged){
-            ctx.fillStyle="gray";
-        }else{
-            ctx.fillStyle=color;
-        }
-        ctx.beginPath();
-        ctx.moveTo(this.polygon[0].x,this.polygon[0].y);
-        for(let i=1;i<this.polygon.length;i++){
-            ctx.lineTo(this.polygon[i].x,this.polygon[i].y);
-        }
-        ctx.fill();
+        ctx.save();
+        ctx.translate(this.x,this.y);
+        ctx.rotate(-this.angle);
+        ctx.drawImage(this.img,
+            -this.width/2,
+            -this.height/2,
+            this.width,
+            this.height);
+        ctx.restore();
 
-        if(this.sensor)
+        if(this.sensor){
             this.sensor.draw(ctx);
+        }
     }
 }
